35eff4504b82ff2c16862b515d136e5455638cb2
Brian Brotherton <bbrotherton@virtualhold.con>
2014-08-29 09:24:02 -0400

    Fix naming of monkeypatch for log4r and RuntimeError

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -1,9 +1,9 @@
+require 'exception_ext'
+
 require 'utilities/csv_helpers'
 require 'utilities/yaml_helpers'
 require 'utilities/file_system_helpers'
 require 'utilities/logging_helpers'
-
-require 'utilities/error_monkeypatch'
 
 module Utilities
 
